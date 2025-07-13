07d2e4df3d2798edef08f97c7b0d24f35b9582d5
brac24-UbuntuHome <2brac4@gmail.com>
2025-07-12 20:01:16 -0700

    remove venv

---
+++ Course1_Beginning_Custom_Projects_Raspberry_Pi/flask_project/create_venv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# the second venv is the name of the virtual environment. I could have said my_venv but naming it venv is convention
+# running the command below will create a directory called venv as well
+# to run the virtual environment you need to run: source venv/bin/activate
+# to quit the venv run: decactivate from within the venv
+python3 -m venv venv
