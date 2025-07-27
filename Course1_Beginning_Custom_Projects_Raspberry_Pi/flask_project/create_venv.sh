9e3d58220afb6a571afefa37cbfdace6e90b2687
Christian Bracamontes <2brac4@gmail.com>
2025-07-27 15:36:05 -0700

    added comments to script

--- Course1_Beginning_Custom_Projects_Raspberry_Pi/flask_project/create_venv.sh
+++ Course1_Beginning_Custom_Projects_Raspberry_Pi/flask_project/create_venv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# creates a python virtual environment.
+# this script should only be ran once.
+# if you already have a venv directory then no need to run this script
+
 # the second venv is the name of the virtual environment. I could have said my_venv but naming it venv is convention
 # running the command below will create a directory called venv as well
 # to run the virtual environment you need to run: source venv/bin/activate
